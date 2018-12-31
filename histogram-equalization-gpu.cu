9d2e9eafed50880175ac520b8893bcf6d8de4a4d
abdelrahman hamdy metwaly <abdohamdymetwaly@gmail.com>
2018-12-31 22:11:04 +0200

    fixing name kernal to kernel

--- histogram-equalization-gpu.cu
+++ histogram-equalization-gpu.cu
@@ -3,7 +3,7 @@ using namespace std;
 
 
 __global__ 
-void histogram_kernal(int * hist_out, unsigned char * img_in, int img_size){   
+void histogram_kernel(int * hist_out, unsigned char * img_in, int img_size){   
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     
     if (i < img_size) atomicAdd(&hist_out[img_in[i]], 1);    
@@ -11,7 +11,7 @@ void histogram_kernal(int * hist_out, unsigned char * img_in, int img_size){
 
 
 __global__
-void clean_kernal(int *hist_out,int size){
+void clean_kernel(int *hist_out,int size){
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     
     if (i < size) hist_out[i] = 0;    
@@ -29,13 +29,13 @@ void histogram_gpu(int * hist_out, unsigned char * img_in, int img_size, int nbr
     
     int threadsPerBlock = 256;
     int blocksPerGrid =(nbr_bin + threadsPerBlock - 1) / threadsPerBlock;
-    clean_kernal<<<blocksPerGrid, threadsPerBlock>>>(d_hist_out, nbr_bin);
+    clean_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_hist_out, nbr_bin);
     
     cudaDeviceSynchronize();
     
     threadsPerBlock = 256;
     blocksPerGrid =(img_size + threadsPerBlock - 1) / threadsPerBlock;
-    histogram_kernal<<<blocksPerGrid, threadsPerBlock>>>(d_hist_out, d_img_in, img_size);
+    histogram_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_hist_out, d_img_in, img_size);
 
     cudaDeviceSynchronize();
 
